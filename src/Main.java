1f21580c968c728c4e8b424bdb211a9bdafd99ec
Aryan-Kain <Aryanarjun1168@gmail.com>
2026-02-23 09:23:09 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,92 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      PALINDROME CHECKER APP");
+        System.out.println("      Version 1.0.0");
+        System.out.println("=================================\n");
+
+        String input = "madam";
+
+        if (input.equals(new StringBuilder(input).reverse().toString())) {
+            System.out.println("UC2: " + input + " is Palindrome");
+        } else {
+            System.out.println("UC2: " + input + " is NOT Palindrome");
+        }
+
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("UC3: " + input + " is Palindrome");
+        } else {
+            System.out.println("UC3: " + input + " is NOT Palindrome");
+        }
+
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println("UC4: " + input + (isPalindrome ? " is Palindrome" : " is NOT Palindrome"));
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        String stackReverse = "";
+        while (!stack.isEmpty()) {
+            stackReverse += stack.pop();
+        }
+
+        System.out.println("UC5: " + input + (input.equals(stackReverse) ? " is Palindrome" : " is NOT Palindrome"));
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack2.push(c);
+        }
+
+        boolean result = true;
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack2.pop())) {
+                result = false;
+                break;
+            }
+        }
+
+        System.out.println("UC6: " + input + (result ? " is Palindrome" : " is NOT Palindrome"));
+
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean dequeResult = true;
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                dequeResult = false;
+                break;
+            }
+        }
+
+        System.out.println("UC7: " + input + (dequeResult ? " is Palindrome" : " is NOT Palindrome"));
     }
 }
